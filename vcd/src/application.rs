435a01e29735f5d85c4c6b7fdb846203b9a2ae4a
Jacek Ruszecki <jacek.ruszecki@eltem.pl>
2025-08-22 07:44:00 +0200

    Application added

---
+++ vcd/src/application.rs
@@ -0,0 +1,81 @@
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{
+    layout::Margin, style::{Color, Modifier, Style, Stylize}, text::Line, widgets::{
+        Block, Borders, Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState,
+    }, DefaultTerminal, Frame
+};
+use std::
+    io::{self}
+;
+
+pub struct Application {
+    counter: u8,
+  
+}
+
+impl Application {
+    pub fn new() -> Self {
+        Application {
+            counter: 0,
+        }
+    }
+
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+            match event::read()? {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    match key_event.code {
+                        KeyCode::Char('q') => break,
+                        KeyCode::Enter => break,
+                        _ => {}
+                    }
+                }
+                _ => {}
+            };
+        }
+        Ok(())
+    }
+
+    fn draw(&mut self, frame: &mut Frame) {
+        let items: Vec<Line> = (1..100)
+            .map(|i| {
+                let line = Line::from(format!(
+                    "├─────────────────┬──┐ ▸ Baaaaaaaaaardzo dłuuuuuuga linia {}",
+                    i
+                ));
+
+                if i == 65 {
+                    line.bg(Color::Blue)
+                } else {
+                    line
+                }
+            })
+            .collect();
+
+        let par = Paragraph::new(items)
+            .block(
+                Block::default()
+                    .title("Directory navigator")
+                    .title_bottom("/home/jacek")
+                    .title_style(Style::default().add_modifier(Modifier::REVERSED))
+                    .borders(Borders::ALL)
+                    .border_style(Style::new().gray()),
+            )
+            .scroll((50, 0));
+        frame.render_widget(par, frame.area());
+
+        let mut scrollbar_state = ScrollbarState::new(99).position(50);
+        // .viewport_content_length(50);
+
+        let scroll = Scrollbar::new(ScrollbarOrientation::VerticalRight);
+        frame.render_stateful_widget(
+            scroll,
+            frame.area().inner(Margin {
+                vertical: 1,
+                horizontal: 0,
+            }),
+            &mut scrollbar_state,
+        );
+    }
+}
--- vcd/src/main.rs
+++ vcd/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod application;
+
+use std::io;
+use application::Application;
+
+fn main()-> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let mut app = Application::new();
+    let res = app.run(&mut terminal);
+    ratatui::restore();
+    res 
 }
